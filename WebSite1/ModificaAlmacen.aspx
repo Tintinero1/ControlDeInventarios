30d5eb8e91da367d1b139fe815b06a89a090aacf
Abel Pech <abel.pech@gmail.com>
2019-12-04 14:25:36 -0800

    ModificaAlmacen

---
+++ WebSite1/ModificaAlmacen.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ModificaAlmacen.aspx.cs" Inherits="ModificaCliente" MasterPageFile="~/MasterPage.Master"%>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="BodyContent" runat="server">
+    <div style="margin-top:100px;">
+        <asp:Label ID="lbIdCliente" runat="server" Text="ID Almacen:"></asp:Label>
+        <asp:TextBox ID="txtIdAlmacen" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lbNombre" runat="server" Text="Nombre del Almacen:"></asp:Label>
+        <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lbActivo" runat="server" Text="Estatus: "></asp:Label>
+        <asp:DropDownList ID="ddlEstatus" runat="server" Width="125px">
+            <asp:ListItem>Inactivo</asp:ListItem>
+            <asp:ListItem>Activo</asp:ListItem>
+        </asp:DropDownList>
+
+        <asp:GridView ID="gvClientes" runat="server" AutoGenerateColumns="true"></asp:GridView>
+    </div>
+    <div>
+        <asp:Button ID="btnModificaAlmacen" runat="server" Text="Modificar" Width="305px" OnClick="btnModificaAlmacen_Click" />
+    </div>
+    
+</asp:Content>
---
+++ WebSite1/ModificaAlmacen.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebSite1;
+
+public partial class ModificaCliente : System.Web.UI.Page
+{
+    BL bl = new BL();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            RefreshTableData();
+        }
+        RefreshTableData();
+    }
+    public void RefreshTableData()
+    {
+        gvClientes.DataSource = bl.ConsultaAlmacen(" ", " 1");
+        gvClientes.DataBind();
+        /*ListViewClientes.DataSource = bl.getTable("cliente");
+        ListViewClientes.DataBind();
+        Repeater1.DataSource = bl.getTable("cliente");
+        Repeater1.DataBind();*/
+    }
+
+    protected void btnModificaAlmacen_Click(object sender, EventArgs e)
+    {
+        string idAlmacen = txtIdAlmacen.Text;
+        string nombre = txtNombre.Text;
+        
+        string estatus;
+        if (ddlEstatus.SelectedValue =="Activo")
+        {
+             estatus = "1";
+        }
+        else 
+        {
+            estatus = "0";
+        }
+
+        bl.ModificaAlmacen(idAlmacen,nombre, estatus, "root");
+        RefreshTableData();
+        
+    }
+}
